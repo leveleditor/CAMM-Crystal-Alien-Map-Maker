21ea726e0c19f9e81c14127c8994aec40526169d
Josh <leveleditor@users.noreply.github.com>
2015-02-15 21:26:50 -0500

    Cleaned up the Tile class.

--- CAMM (Crystal Alien Map Maker)/CAMM (Crystal Alien Map Maker)/Tile.vb
+++ CAMM (Crystal Alien Map Maker)/CAMM (Crystal Alien Map Maker)/Tile.vb
@@ -1,33 +1,29 @@
 ﻿Public Class Tile
 
-    Public Sub New(ByVal X As Integer, ByVal Y As Integer)
-        Me.Position = New Point(X, Y)
+    Public Sub New(ByVal x As Integer, ByVal y As Integer)
+        Me.Position = New Point(x, y)
         Me.Image = Nothing
         Me.TileId = -1
         Me.IsPassable = False
         Me.IsMinerals = False
     End Sub
-    Public Sub New(ByVal X As Integer, ByVal Y As Integer, ByVal Image As Image)
-        Me.New(X, Y)
-        Me.Image = Image
+    Public Sub New(ByVal x As Integer, ByVal y As Integer, ByVal image As Image)
+        Me.New(x, y)
+        Me.Image = image
     End Sub
-    Public Sub New(ByVal X As Integer, ByVal Y As Integer, ByVal Image As Image, ByVal TileId As String)
-        Me.New(X, Y, Image)
-        Me.TileId = TileId
+    Public Sub New(ByVal x As Integer, ByVal y As Integer, ByVal image As Image, ByVal tileId As String)
+        Me.New(x, y, image)
+        Me.TileId = tileId
     End Sub
-    Public Sub New(ByVal X As Integer, ByVal Y As Integer, ByVal Image As Image, ByVal TileId As String, ByVal IsPassable As Boolean, ByVal IsMinerals As Boolean)
-        Me.New(X, Y, Image, TileId)
-        Me.IsPassable = IsPassable
-        Me.IsMinerals = IsMinerals
+    Public Sub New(ByVal x As Integer, ByVal y As Integer, ByVal image As Image, ByVal tileId As String, ByVal isPassable As Boolean, ByVal isMinerals As Boolean)
+        Me.New(x, y, image, tileId)
+        Me.IsPassable = isPassable
+        Me.IsMinerals = isMinerals
     End Sub
 
     Public ReadOnly Property HasData() As Boolean
         Get
-            Dim value As Boolean = False
-            If Image IsNot Nothing Then
-                value = True
-            End If
-            Return value
+            Return Image IsNot Nothing
         End Get
     End Property
 
@@ -36,7 +32,7 @@
             Return Position.X
         End Get
         Set(ByVal value As Integer)
-            pt_Position.X = value
+            _position.X = value
         End Set
     End Property
 
@@ -45,58 +41,26 @@
             Return Position.Y
         End Get
         Set(ByVal value As Integer)
-            pt_Position.Y = value
+            _position.Y = value
         End Set
     End Property
 
-    Private pt_Position As Point
+    Private _position As Point
     Public Property Position() As Point
         Get
-            Return pt_Position
+            Return _position
         End Get
         Set(ByVal value As Point)
-            pt_Position = value
+            _position = value
         End Set
     End Property
 
-    Private _image As Image
-    Public Property Image() As Image
-        Get
-            Return _image
-        End Get
-        Set(ByVal value As Image)
-            _image = value
-        End Set
-    End Property
+    Public Property Image As Image
 
-    Private str_Id As String = -1
-    Public Property TileId() As String
-        Get
-            Return str_Id
-        End Get
-        Set(ByVal value As String)
-            str_Id = value
-        End Set
-    End Property
+    Public Property TileId As String = -1
 
-    Private b_IsPassable As Boolean = False
-    Public Property IsPassable() As Boolean
-        Get
-            Return b_IsPassable
-        End Get
-        Set(ByVal value As Boolean)
-            b_IsPassable = value
-        End Set
-    End Property
+    Public Property IsPassable As Boolean = False
 
-    Private b_IsMinerals As Boolean = False
-    Public Property IsMinerals() As Boolean
-        Get
-            Return b_IsMinerals
-        End Get
-        Set(ByVal value As Boolean)
-            b_IsMinerals = value
-        End Set
-    End Property
+    Public Property IsMinerals As Boolean = False
 
 End Class
