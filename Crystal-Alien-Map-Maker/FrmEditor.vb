654464edf5eebc9193f08afac76e1b04f5573379
Josh <leveleditor@users.noreply.github.com>
2016-10-22 17:22:52 -0400

    Fixed rectangle brush tool drag from bottom-right to top-left

--- Crystal-Alien-Map-Maker/FrmEditor.vb
+++ Crystal-Alien-Map-Maker/FrmEditor.vb
@@ -366,8 +366,19 @@ Public Class FrmEditor
         End If
 
         If ActiveEditMode = EditMode.Tiles And ActiveToolMode = ToolMode.RectangleBrush And Not My.Computer.Keyboard.CtrlKeyDown And rectSelectStartX <> -1 And rectSelectStartY <> -1 Then
+            Dim startx, starty, endx, endy As Single
+            startx = rectSelectStartX
+            starty = rectSelectStartY
+            endx = mouseX
+            endy = mouseY
+            Dim rect As Rectangle = New Rectangle(
+                Math.Min(startx, endx),
+                Math.Min(starty, endy),
+                Math.Abs(endx - startx),
+                Math.Abs(endy - starty)
+            )
             'Fill the selected rectangle with the data from the selected brush preset.
-            ActiveMap.SetTileRectangle(rectSelectStartX, rectSelectStartY, mouseX, mouseY, cboRectangleBrush.SelectedItem)
+            ActiveMap.SetTileRectangle(rect.X, rect.Y, rect.Right, rect.Bottom, cboRectangleBrush.SelectedItem)
 
             'Reset starting point for rectangle brush selection.
             rectSelectStartX = -1
@@ -450,8 +461,32 @@ Public Class FrmEditor
                     If ActiveToolMode = ToolMode.Eraser Or My.Computer.Keyboard.CtrlKeyDown Then
                         g.DrawRectangle(PenTileErase, mouseX - (PenTileHover.Width / 2), mouseY - (PenTileHover.Width / 2), TileSizeX + PenTileHover.Width + 1, TileSizeY + PenTileHover.Width + 1)
                     ElseIf ActiveToolMode = ToolMode.RectangleBrush And rectSelectStartX <> -1 And rectSelectStartY <> -1 Then
-                        g.DrawRectangle(PenTileHover, rectSelectStartX - (PenTileHover.Width / 2), rectSelectStartY - (PenTileHover.Width / 2), mouseX - rectSelectStartX + TileSizeX + PenTileHover.Width + 1, mouseY - rectSelectStartY + TileSizeY + PenTileHover.Width + 1)
-                        g.DrawRectangle(PenTileHover, rectSelectStartX + PenTileHover.Width, rectSelectStartY + PenTileHover.Width, mouseX - rectSelectStartX + TileSizeX - PenTileHover.Width - 1, mouseY - rectSelectStartY + TileSizeY - PenTileHover.Width - 1)
+                        Dim startx, starty, endx, endy As Single
+                        If rectSelectStartX <= mouseX Then
+                            startx = rectSelectStartX
+                            endx = mouseX + TileSizeX
+                        Else
+                            startx = rectSelectStartX + TileSizeX
+                            endx = mouseX
+                        End If
+                        If rectSelectStartY <= mouseY Then
+                            starty = rectSelectStartY
+                            endy = mouseY + TileSizeY
+                        Else
+                            starty = rectSelectStartY + TileSizeY
+                            endy = mouseY
+                        End If
+                        Dim rect As Rectangle = New Rectangle(
+                            Math.Min(startx, endx),
+                            Math.Min(starty, endy),
+                            Math.Abs(endx - startx),
+                            Math.Abs(endy - starty)
+                        )
+                        Dim o1, o2 As Single
+                        o1 = (PenTileHover.Width / 2)
+                        o2 = PenTileHover.Width
+                        g.DrawRectangle(PenTileHover, rect.X - o1, rect.Y - o1, rect.Width + PenTileHover.Width + 1, rect.Height + PenTileHover.Width + 1)
+                        g.DrawRectangle(PenTileHover, rect.X + o2, rect.Y + o2, rect.Width - PenTileHover.Width - 1, rect.Height - PenTileHover.Width - 1)
                     ElseIf ActiveEditMode = EditMode.Buildings And activeBuilding.BuildingId <> "" Then
                         g.FillRectangle(BrushBuildingPlacement, mouseX, mouseY, activeBuilding.BuildingW * TileSizeX + 1, activeBuilding.BuildingH * TileSizeY + 1)
                     Else
